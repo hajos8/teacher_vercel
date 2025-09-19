ee0c6a29835da2bfbcaf7725decee91e17f109e4
hajos8 <rabb.andras@gmail.com>
2025-09-19 12:40:18 +0200

    igen

--- react-app/api/teachers.js
+++ react-app/api/teachers.js
@@ -2,6 +2,7 @@ import { global } from './globals.js'
 
 export default async function handler(req, res){
     if(req.method === 'GET'){
+        console.log('params', req.params)
         res.status(200).json(global.teachers)
     }
 }
